2a316d6bc05c142f9e023539c49b4687b6043640
Professor Wergeles <wergelesn@missouri.edu>
2019-10-04 00:10:43 -0500

    Code After Class

--- 2. AngularMVC/src/app/app.component.ts
+++ 2. AngularMVC/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import planets from "../assets/planetData.json"; 
+
+import { Planets } from "src/planets";
 
 @Component({
   selector: 'app-root',
@@ -9,10 +12,41 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularMVC';
 
+  data: Array<Planets> = planets; 
+
+  totalPlanets: number = 8; 
+
+  views = ["Select One", "list", "table", "system", "all"]; 
+
+  selection;
+  formPlanetName;  
+
   constructor(){
+    console.log(this.data); 
+    this.selection = "Select One"; 
   }
 
   ngOnInit(){
-    
+    this.data = (localStorage.getItem('planets') !== null) ? 
+                  JSON.parse(localStorage.getItem('planets')) :
+                  planets
+  }
+
+  getTotalPlanets(){
+    return this.data.length; 
+  }
+  addMyPlanet(){
+    this.data.push({
+      name: this.formPlanetName, 
+      color: "gray", 
+      radius: 1186, 
+      satellites: 5
+    });
+    console.log(this.data); 
+
+    localStorage.setItem('planets', JSON.stringify(this.data)); 
+  }
+  selected(){
+    console.log(this.selection);
   }
 }
